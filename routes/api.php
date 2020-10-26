0354bbd92f6030f2e214a55bf381ae2fafa6b7f3
kafribung <60900117001@uin-alauddin.ac.id>
2020-10-26 19:04:24 +0800

    route prefix

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::namespace('Auth')->group(function(){
     Route::post('register',  'RegisterController');
     Route::post('login', 'LoginController');
     Route::post('logout', 'LogoutController');
 });
 
-Route::namespace('Post')->middleware('auth:api')->group(function(){
-    Route::get('post', 'PostController@index');
-    Route::post('post', 'PostController@store');
-    Route::get('post/{post:slug}', 'PostController@show');
-    Route::put('post/{post:slug}', 'PostController@update');
-    Route::delete('post/{post:slug}', 'PostController@destroy');
+Route::namespace('Post')->middleware('auth:api')->prefix('post')->group(function(){
+    Route::get('', 'PostController@index');
+    Route::post('', 'PostController@store');
+    Route::get('/{post:slug}', 'PostController@show');
+    Route::put('/{post:slug}', 'PostController@update');
+    Route::delete('/{post:slug}', 'PostController@destroy');
 });
 
 
